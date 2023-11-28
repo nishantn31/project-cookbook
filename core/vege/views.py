a32c88c2c582ad3c2e2ddd64c196068055642ab5
Nishant Nayan <nishantnayan@Nishants-MacBook-Air.local>
2023-11-28 21:43:06 +0530

    modified UI for better user experience

--- core/vege/views.py
+++ core/vege/views.py
@@ -102,33 +102,30 @@ def logout_page(request):
 
 def register_page(request):
 
-    if request.method == "POST":
-        first_name = request.POST.get("first_name")
-        last_name = request.POST.get("last_name")
-        username = request.POST.get("username")
-        password = request.POST.get("password")
+    if request.method != "POST":
+        return render(request, "register.html")
+    first_name = request.POST.get("first_name")
+    last_name = request.POST.get("last_name")
+    username = request.POST.get("username")
+    password = request.POST.get("password")
 
-        user = User.objects.filter(username = username)
+    user = User.objects.filter(username = username)
 
-        if user.exists():
-            messages.info(request, "Username already exists, please create new one.")
-            return redirect("/register/")
-
-
-
-        user = User.objects.create(
-            first_name = first_name,
-            last_name = last_name,
-            username = username,
-          )
-        
-        user.set_password(password)
-        user.save()
-
-        messages.info(request, "Account created successfully!")
-
+    if user.exists():
+        messages.info(request, "Username already exists, please create new one.")
         return redirect("/register/")
 
 
 
-    return render(request, "register.html")
+    user = User.objects.create(
+        first_name = first_name,
+        last_name = last_name,
+        username = username,
+      )
+
+    user.set_password(password)
+    user.save()
+
+    messages.info(request, "Account created successfully!")
+
+    return redirect("/register/")
